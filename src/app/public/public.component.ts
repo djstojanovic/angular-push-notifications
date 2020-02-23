a230c85d81cb9a989f2b6130d86cb20dbe4bd149
Djordje Stojanovic <dj.stojanovic@levi9.com>
2020-02-24 00:16:44 +0100

    updated styles. added click event handler on notifications. Note: https://stackoverflow.com/questions/54138763/open-pwa-when-clicking-on-push-notification-handled-by-service-worker-ng7-andr

--- src/app/public/public.component.ts
+++ src/app/public/public.component.ts
@@ -1,41 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { SwPush } from '@angular/service-worker';
-import { PushNotificationService } from '../shared/push-notification-service/push-notification.service';
-import { Config } from '../config/config';
+import { Component, OnInit } from "@angular/core";
+import { SwPush } from "@angular/service-worker";
+import { PushNotificationService } from "../shared/push-notification-service/push-notification.service";
+import { Config } from "../config/config";
 
 @Component({
-  selector: 'app-public',
-  templateUrl: './public.component.html',
-  styleUrls: ['./public.component.scss']
+  selector: "app-public",
+  templateUrl: "./public.component.html",
+  styleUrls: ["./public.component.scss"]
 })
 export class PublicComponent implements OnInit {
-
   icon: string = "/assets/icons/logo.png";
 
-  constructor(private swPush: SwPush,
-    private pushService: PushNotificationService) {
-    console.log('swPush.isEnabled', swPush.isEnabled)
+  constructor(
+    private swPush: SwPush,
+    private pushService: PushNotificationService
+  ) {
+    console.log("swPush.isEnabled", swPush.isEnabled);
   }
 
   ngOnInit(): void {
+    this.requestSubscription();
+  }
+
+  requestSubscription() {
     if (this.swPush.isEnabled) {
-      this.swPush.requestSubscription({
-        serverPublicKey: Config.publicKey
-      }).then(subscription => this.handleSubscription(subscription))
+      this.swPush
+        .requestSubscription({
+          serverPublicKey: Config.publicKey
+        })
+        .then(subscription => this.handleSubscription(subscription))
         .catch(error => this.handleSubscriptionError(error));
     } else {
-      console.error('Push notifications not available');
+      console.error("Push notifications not available");
     }
   }
 
-  
+
   handleSubscription(subscription) {
-    console.log(subscription);
-    this.pushService.sendSubscriptionToTheServer(subscription).subscribe((message) => {
-      console.log(message);
-    }, (error) => {
-      console.error(error);
-    });
+    this.pushService.sendSubscriptionToTheServer(subscription).subscribe(
+      message => {
+        console.log(message);
+      },
+      error => {
+        console.error(error);
+      }
+    );
   }
 
   handleSubscriptionError(error) {
